9c10b1bd4240f26fe4e56aec0c5faf94a693d2a0
Nguyen-Cong-Nghiem <40155344+Nguyen-Cong-Nghiem@users.noreply.github.com>
2018-12-05 09:35:43 +0700

    Refactor source code with annotation lombok

--- src/main/java/Programmer.java
+++ src/main/java/Programmer.java
@@ -1,89 +1,14 @@
+import lombok.*;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@ToString
+@Builder
 public class Programmer {
     private String id;
     private String name;
     private byte age;
-
-    public Programmer() {
-    }
-
-    public Programmer(String id, String name, byte age) {
-        this.id = id;
-        this.name = name;
-        this.age = age;
-    }
-
-    public String getId() {
-        return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public byte getAge() {
-        return age;
-    }
-
-    public void setAge(byte age) {
-        this.age = age;
-    }
-
-    @Override
-    public String toString() {
-        return "Programmer{" +
-                "id='" + id + '\'' +
-                ", name='" + name + '\'' +
-                ", age=" + age +
-                '}';
-    }
-
-    public static ProgrammerBuilder builder() {
-        return new ProgrammerBuilder();
-    }
-
-    public static class ProgrammerBuilder {
-        private String id;
-        private String name;
-        private byte age;
-
-        ProgrammerBuilder() {
-        }
-
-        public Programmer.ProgrammerBuilder id(String id) {
-            this.id = id;
-            return this;
-        }
-
-        public Programmer.ProgrammerBuilder name(String name) {
-            this.name = name;
-            return this;
-        }
-
-        public Programmer.ProgrammerBuilder age(byte age) {
-            this.age = age;
-            return this;
-        }
-
-        public Programmer build() {
-            return new Programmer(id, name, age);
-        }
-
-        @Override
-        public String toString() {
-            return "Programmer.ProgrammerBuilder{" +
-                    "id='" + id + '\'' +
-                    ", name='" + name + '\'' +
-                    ", age=" + age +
-                    '}';
-        }
-    }
 
 }
